94f702aaa13cdafe2095cdf99c3cc2676c7f9b62
hbpark95 <ci.hongbin95@gmail.com>
2020-05-15 11:14:55 +0900

    service를 호출하는 api 코드 작성

--- HANSUNG.Web/Controllers/API/ContactController.cs
+++ HANSUNG.Web/Controllers/API/ContactController.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using HANSUNG.Common;
+using HANSUNG.Core.Models;
+using System.Web.Mvc;
 
 namespace HANSUNG.Controllers.API
 {
-    public class ContactController
+    public class ContactController : BaseController
     {
+        public JsonResult GetContactList(RouteGetContactList route)
+        {
+            ViewModelContactList vmodel = new ViewModelContactList();
+
+            _contactService.GetContactList(ref vmodel, ref route);
+
+            return Json(vmodel, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ContactList()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateAllRecord()
+        {
+
+            return View();
+        }
     }
 }
